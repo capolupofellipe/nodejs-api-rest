81b132322bd025552e3c23779910948c9beb537a
Fellipe Capolupo <fellipe.capolupo@kinvo.com.br>
2021-10-11 17:22:49 -0300

    adds new verbs

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,6 +1,14 @@
 const Atendimento = require('../models/atendimentos');
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um get'));
+    app.get('/atendimentos', (req, res) => {
+         Atendimento.lista(res);
+        });
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.obtem(res, id);
+    }) 
+
 
     app.post('/atendimentos', (req,res) => 
     {
@@ -8,5 +16,19 @@ module.exports = app => {
 
         console.log(req.body);
         Atendimento.adiciona(atendimento, res);        
+    });
+
+    app.patch('/atendimentos/:id', (req, res) =>{
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+
+        Atendimento.altera(res, id, valores);
+    });
+
+    app.delete('/atendimentos/:id', (req, res) =>{
+        
+        const id = parseInt(req.params.id);
+        
+        Atendimento.delete(res, id);
     });
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,5 +1,6 @@
 const conexao = require('../infraestrutura/conexao');
 const moment = require('moment');
+const { isBuffer } = require('util');
 
 class Atendimento{
     adiciona(atendimento, res) {
@@ -31,7 +32,7 @@ class Atendimento{
         }        
         
         const atendimentoDatado = {...atendimento, dataCriacao, data};
-        
+
         const sql = 'INSERT INTO Atendimentos SET ?';
 
         conexao.query(sql, atendimentoDatado, (erro, resultado) => {
@@ -41,6 +42,60 @@ class Atendimento{
                 res.status(201).json(resultado);
             }
             
+        })
+    }
+
+    lista(res){
+        const sql = 'select * from atendimentos'
+
+        conexao.query(sql, (erro, resultados) => {
+            if(erro){
+            res.status(400).json(erro);
+        } else {
+            res.status(200).json(resultados);
+        }
+
+        })
+    }
+
+    obtem(res, id){
+        const sql = `select * from atendimentos where id = ${id}`;
+        
+        conexao.query(sql, (erro, resultado) => {
+            const atendimento = resultado[0];
+            if(erro){
+                res.status(400).json(erro);
+            } else { 
+                res.status(200).json(atendimento);
+            }
+        })
+    }
+
+    altera(res, id, valores){
+        const sql = 'update atendimentos set ? where id =?'
+        
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss'); 
+        }
+
+        conexao.query(sql, [valores, id], (erro, resultado) => {
+            if(erro){
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultado);
+            }
+        });
+    }
+
+    delete(res, id){
+        const sql = 'delete from atendimentos where id=?';
+
+        conexao.query(sql, id, (erro, resultado) => {
+            if(erro){
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultado);
+            }
         })
     }
 }
