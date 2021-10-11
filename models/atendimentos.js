c2f79dcc08c751f74b5b103ef58c967c5f96a8ea
Fellipe Capolupo <fellipe.capolupo@kinvo.com.br>
2021-10-11 16:31:37 -0300

    adds business logic

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,9 +5,32 @@ class Atendimento{
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
         
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
-        const atendimentoDatado = {...atendimento, dataCriacao, data};
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');        
         
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                mensagem: 'data deve ser maior que data atual',
+                valido: dataEhValida
+            },
+            {
+                nome: 'cliente',
+                mensagem: 'cliente deve ter mais que 5 caracteres',
+                valido: clienteEhValido
+            },
+        ];
+        
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if(existemErros){
+            res.status(400).json(erros);
+        }        
+        
+        const atendimentoDatado = {...atendimento, dataCriacao, data};
         
         const sql = 'INSERT INTO Atendimentos SET ?';
 
